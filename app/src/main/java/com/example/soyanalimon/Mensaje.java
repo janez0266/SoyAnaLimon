b6490774c51fc548fa0e67998770da1f4ffa7528
Julio Añez <janez0266@gmail.com>
2021-01-31 20:33:33 -0300

    modificacion de mensajes

--- app/src/main/java/com/example/soyanalimon/Mensaje.java
+++ app/src/main/java/com/example/soyanalimon/Mensaje.java
@@ -1,7 +1,7 @@
 package com.example.soyanalimon;
 
 public class Mensaje {
-    String mensajeBienvenida = "varias palabras";
+    String mensajeBienvenida = "varias palabrasxxx";
     String mensajeError = "Error de Procedimiento";
 
 }
